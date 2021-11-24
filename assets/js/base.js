c87941bf1aa5b6f4a22f01cc5d4f88ec67afbc72
Ivonne <ivonne.scarlett01@gmail.com>
2021-11-23 22:47:38 -0600

    agregar isNaN a if else

--- assets/js/base.js
+++ assets/js/base.js
@@ -2,13 +2,18 @@ let number1 = parseInt(prompt('Introduce el primer número que requieres'))
 let number2 = parseInt(prompt('Introduce el segundo número que requieres'))
 let result = 0
 
-if(number1 > number2){
-    result = number1 / number2
-    console.log('La division es:', result)
-}else if(number1 < number2){
-    result = number1 + number2
-    console.log('La suma es:', result)
-}else if(number1==number2){
-    result = number1 * number2
-    console.log('La multiplicacion es:', result)
+if (isNaN(number1) === false && isNaN(number2) === false){
+    if(number1 > number2){
+        result = number1 / number2
+        console.log('La division es:', result)
+    }else if(number1 < number2){
+        result = number1 + number2
+        console.log('La suma es:', result)
+    }else if(number1==number2){
+        result = number1 * number2
+        console.log('La multiplicacion es:', result)
+    }
+} else{
+    console.error('Algun dato introducido no es número')
 }
+
