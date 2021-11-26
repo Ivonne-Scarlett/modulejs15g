36f8f3cecdd25d0f6b82c8a3d71075cd789186cf
Ivonne <ivonne.scarlett01@gmail.com>
2021-11-25 22:49:41 -0600

    for, while

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,66 +1,114 @@
-let grados = prompt('Introduce los grados Celsius actuales de tu Localidad')
-let result = 0
+/*
+let saludo = 'Hola'
 
-result = (grados * 1.8) + 32
-alert(result)
-//alert(`La conversión es: ${result} °F`)
+for (i = 0; i < saludo.length; i++){
+    console.log(saludo[i]) //devuelve h,o,l,a
+    //console.log(i)  //devuelve 1, 2, 3, 4
+}
+*/
+
+//Pedir al usuario un string
+//contar las vocales y consonantes
+//imprimir en consola
+//2 vocales y 2 consonantes
+
+/*
+let text = prompt("Ingresa un texto o línea de texto")
+let text = text.toLowerCase().length()
+console.log(text)
+if (text == a || text == e || text == i || text == o || text == u ){
+    for (i = 0; i < text.length; i++){
+        console.log(text[i]) 
+    }
+
+} else {
+    for (i = 0; i < text.length; i++){
+        console.log(text[i]) 
+    }
+}
+*/
+
+//pedir una palabra con espacio al inicio y al final
+//contar las letras a, e
+//contar número de espacios, sin contar lo espacios de inicio y de fin
+
+/*
+let text = prompt("Ingresa un texto o línea de texto").toLowerCase()
+let result = text.trim()
+let lettera = 0
+let lettere = 0
+let space = 0
+
+for (i = 0; i < result.length; i++){    
+    if ( result[i] == "a" ){
+        lettera++
+    } else if ( result[i] == "e" ){
+        lettere++
+    } else if ( result[i] == " " ){
+        space++
+    }
+}
+console.log(`Existen ${lettera} letras "a", ${lettere} letras "e" y ${lettere} espacios `)
+*/
 
 
 
-const numberone = parseInt(prompt("ingresa el primer número"))
-const numbertwo = parseInt(prompt("ingresa el segundo número"))
-const numberthree = parseInt(prompt("ingresa el tercer número"))
-if (!isNaN(numberone) && !isNaN(numbertwo) && !isNaN(numberthree)) {
-    if (numberone > numbertwo && numberone>numberthree){
-        console.log(`${numberone} es el mayor`)
-    }else if (numbertwo > numberone && numbertwo>numberthree) {
-        console.log(`${numbertwo} es el mayor`)
-    }else if (numberthree> numberone && numberthree>numbertwo) {
-        console.log(`${numberthree} es el mayor`)
-    }else {
-        console.log('Los numeros son iguales')
+//pedir al usuario un numero del 1 al 100
+//sumar todos los número
+//mandar un alert con el total
+/*let number = parseInt(prompt("Ingresa un número"))
+let j = 0
+for(let i=1; i < number; i++) {   
+    j += i    
+}
+alert(j)
+*/
+
+/*
+let number = parseInt(prompt("Ingresa un número"))
+let suma = 0
+let counter = 1
+
+if(!isNaN(number) && number >=1 && number <=100){
+    while(counter <= number){
+        suma = suma + counter
+        counter = counter + 1
     }
 } else {
-    console.error('Ingresa un número, los datos introducidos no corresponden a datos numericos')
+    console.log('No es un número valido')
+}
+console.log(suma)
+*/
+
+
+
+//pedir al usuario una palabra (permitir espacios, mayusculas)
+//Verificar que sea palindrome
+//Mostrar en consola: la palabra X es un palindrome
+
+let palindrome = prompt("Ingresa un texto").toLowerCase().trim()
+let comparacion = ''
+
+palindrome.charAt(0)
+console.log(palindrome)
+for (i = 0; i < palindrome.length; i++){
+        
+    
+
+    
 }
 
+//console.log(palindrome[i])
 
 
-const clima = prompt("¿Cómo esta el clima en tu ciudad? Ingresa soleado, lluvioso, nevando o nublado")
-const gradosC = parseInt(prompt("Ingresa la temperatura de tu Ciudad"))
-let convertGrados = 0
-convertGrados = (gradosC * 1.8) + 32
-
-if (!isNaN(gradosC)) {
-    switch(clima){
-        case 'soleado':
-        case 'Soleado':
-        case 'SOLEADO':  
-            console.log(`En tu ciudad el día esta ${clima}, con una temperatura de ${convertGrados}°F`)
-            break
-        case 'lluvioso':
-        case 'Lluvioso':
-        case 'LLUVIOSO':  
-            console.log(`En tu ciudad el día esta ${clima}, con una temperatura de ${convertGrados}°F`)
-            break
-        case 'nevando':
-        case 'Nevando':
-        case 'NEVANDO':  
-            console.log(`En tu ciudad el día esta ${clima}, con una temperatura de ${convertGrados}°F`)
-            break
-        case 'nublado':
-        case 'Nublado':
-        case 'NUBLADO':  
-            console.log(`En tu ciudad el día esta ${clima}, con una temperatura de ${convertGrados}°F`)
-            break        
-        default:
-        console.log('Opción ingresada no permitida')
-    }
-} else{
-    console.log('La temperatura debe ser ingresada en número')
-}
 
 
+/*
+do{
+
+}while()
+*/
+ 
 
 
 
