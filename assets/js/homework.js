9997c829bfa4081bb888dc9532279217b4cd0d05
Ivonne <ivonne.scarlett01@gmail.com>
2021-11-26 19:29:52 -0600

    Tarea

---
+++ assets/js/homework.js
@@ -0,0 +1,53 @@
+// For solution
+/*
+const wordUser = prompt('Dame una palabra ').trim().toLowerCase()
+
+let wordWithoutSpace = ''
+let wordReversed = ''
+
+if(wordUser != ''){
+    // Remove White space
+    for(i = 0; i < wordUser.length; i++){
+        if(wordUser[i] !== ' '){
+            wordWithoutSpace = wordWithoutSpace +  wordUser[i]
+        }
+    }
+    console.log(wordWithoutSpace)
+
+    // word reversed  
+    /*
+    for(j = wordWithoutSpace.length - 1; j >= 0 ; j--){
+        wordReversed = wordReversed + wordWithoutSpace[j]
+    } 
+    console.log(wordReversed)
+    */
+   /*while (j >= 0){
+        let j = wordWithoutSpace.length - 1
+        wordReversed = wordReversed + wordWithoutSpace[j]
+        j--       
+   }
+
+
+    wordWithoutSpace === wordReversed ?
+        console.log(`La palabra "${wordUser}" es un palindromo`)
+    :
+        console.log(`La palabra "${wordUser}" No es un palindromo`)
+
+} else {
+    console.log('Inserta una palabra valida')
+}
+*/
+
+//Imprimir en consola la suma de multiplos de 5 y 3
+
+var multiplo3 = 0
+var multiplo5 = 0
+
+for (i = 100; i <= 1; i--){
+    if(i % 3 == 0){
+        multiplo3 = multiplo3 + i        
+    }else if(i % 5 == 0){
+        multiplo5 = multiplo5 + i
+    }
+}
+console.log(`La suma total de los multiplos de 3 es: ${multiplo3}. La suma total de los multiplos de 5 es: ${multiplo5}`)
