453e99db48f3e85f410d827b479b4ff82fc1d04d
Ivonne <ivonne.scarlett01@gmail.com>
2021-11-29 22:21:25 -0600

    Declarar email

--- assets/js/homework.js
+++ assets/js/homework.js
@@ -1,3 +1,100 @@
+/* 
+ * Ejercicio 1
+ * imprimir en consola todos los numeros pares entre 1 y 100
+ * -> 2
+ * -> 4
+ * -> 100
+*/
+/* 
+console.log(`Los números pares del 1 al 100 son los siguientes:`)
+for (i = 1; i <= 100; i++){
+    if(i % 2 == 0){
+        console.log(i)        
+    }
+}
+*/
+
+
+
+
+
+/*
+ * Ejercicio 2
+ * Pedir al usuario un string
+ * Invertir el orden e imprimir en consola
+ * Entrada: "reverso"
+ * Salida: -> esrever
+ * pista: https://www.w3schools.com/jsref/jsref_length_string.asp
+*/
+/*
+let word = prompt('Ingresa una oración').trim().toLowerCase()
+let wordReverse = ''
+
+for(i = word.length; i >= 0; i--){
+    if(word[i] == ' '){
+        wordReverse += ' ' + word[i - 1]
+        i = i - 1
+    } else {
+        wordReverse = wordReverse + word[i]
+    }
+}
+
+let textReverse = wordReverse.substring(0,1).toUpperCase() + wordReverse.substring(1)
+console.log('la palabra en reversa es:',textReverse)
+*/
+
+
+
+
+
+/*
+ * Ejercicio 3 
+ * Declarar una cadena de al menos 2 palabras
+ * Imprimir en consola el string separado por guiones en cada palabra
+ * "hola koders"
+ * "hola-koders"
+ * pista: https://www.w3schools.com/jsref/jsref_replace.asp
+ */
+/*
+let word = prompt('Ingresa una oración')
+let result = word.replace(' ' ,'-')
+console.log('El texto ingresado es:',result)
+*/
+
+
+
+
+/*
+ * Ejercicio 4
+ * Declarar un string con un correo electrónico
+ * Enmascarar con "*" el bloque del usuario
+ * "jorgec@kodemia.mx"
+* "*** @kodemia .mx"
+ * 
+ * pista: https://www.w3schools.com/jsref/jsref_slice_string.asp
+ * pista: https://www.w3schools.com/jsref/jsref_indexof.asp
+ */
+/*
+let mail = prompt('Ingresa un correo electrónico')
+let encript = ''
+
+for(i = 0; i < mail.length; i++){
+    if(mail[i] == '@'){
+        while(){
+            let result = mail.replace(' ' ,'@')
+        }
+        
+        i = i + 1
+    } else {
+        encript = encript + mail[i]
+    }
+}
+*/
+
+
+
+
+
 // For solution
 /*
 const wordUser = prompt('Dame una palabra ').trim().toLowerCase()
